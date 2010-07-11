5607b59b3af3753ff3ad0d435c2dbc79ebd8848a
Aljeux <aljeux@free.fr>
2010-07-11 11:43:49 +0200

    Add hudson script in scm

---
+++ tests/scripts/fusionforge-github-build-and-test-rpm.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -x
+
+export CONFIG_PHP=func/config.php.buildbot
+export SELENIUM_RC_HOST=192.168.0.204
+export SELENIUM_RC_DIR=$WORKSPACE/reports
+export SELENIUM_RC_URL=${HUDSON_URL}job/$JOB_NAME/ws/reports
+export HOST=centos52.local
+export CONFIGURED=true
+
+mkdir -p build/packages reports/coverage
+make -f Makefile.rh BUILDRESULT=$WORKSPACE/build/packages all
+
+cd tests
+phpunit --log-junit $WORKSPACE/reports/phpunit-selenium.xml RPMCentos52Tests.php
+
+cd ..
+cp $WORKSPACE/reports/phpunit-selenium.xml $WORKSPACE/reports/phpunit-selenium.xml.org; xalan -in $WORKSPACE/reports/phpunit-selenium.xml.org -xsl fix_phpunit.xslt -out $WORKSPACE/reports/phpunit-selenium.xml
