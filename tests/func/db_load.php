23d4b893442326159d7fc361beab1f9f883a1607
Aljeux <aljeux@free.fr>
2010-10-18 20:54:58 +0200

    db_load fix to add grant on schema

--- tests/func/db_load.php
+++ tests/func/db_load.php
@@ -21,7 +21,8 @@ if ( DB_TYPE == 'mysql') {
 	system("mysql -u".DB_USER." -p".DB_PASSWORD." ".DB_NAME." < ".dirname(dirname(dirname(__FILE__)))."/gforge/db/gforge-struct-mysql.sql");
 	system("mysql -u".DB_USER." -p".DB_PASSWORD." ".DB_NAME." < ".dirname(dirname(dirname(__FILE__)))."/gforge/db/gforge-data-mysql.sql");
 } elseif ( DB_TYPE == 'pgsql') {
-	system("psql -q -U".DB_USER." ".DB_NAME." -f ".dirname(dirname(dirname(__FILE__)))."/src/db/reset_schema.sql &>/tmp/gforge-init.log");
+	system("echo \"DROP SCHEMA public CASCADE;CREATE SCHEMA public;\" | psql -q -Upostgres ".DB_NAME." &>/tmp/gforge-init.log");
+	system("echo \"GRANT ALL ON SCHEMA public TO ".DB_USER.";\" | psql -q -Upostgres ".DB_NAME);
 	system("pg_restore ".dirname(__FILE__)."/gforge-init.csql | psql -q -U".DB_USER." ".DB_NAME." &>/tmp/gforge-import.log");
 	system("echo \"UPDATE groups SET homepage='".SITE."/www/projecta/', http_domain='".SITE."/www/projecta/', scm_box='".SITE."' WHERE group_id=6\" | psql -q -U".DB_USER." ".DB_NAME." &>/tmp/gforge-import2.log");
 	system("echo \"VACUUM FULL ANALYZE;\" | psql -q -Upostgres ".DB_NAME);
